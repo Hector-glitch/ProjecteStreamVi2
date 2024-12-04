3cb03f7bb34ad2de660201b607fd10f2d9838d33
Héctor <hector.vinas@institutvidreres.cat>
2024-12-04 22:53:56 +0100

    optimizado

--- src/app/comprovar-c2/comprovar-c2.component.ts
+++ src/app/comprovar-c2/comprovar-c2.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {SocketService} from './socket.service';
+import { FormsModule } from '@angular/forms';
+import { SocketService } from './socket.service';
 
 @Component({
   selector: 'app-comprovar-c2',
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './comprovar-c2.component.html',
   standalone: true,
-  styleUrl: './comprovar-c2.component.css'
+  styleUrls: ['./comprovar-c2.component.css'],
 })
 export class ComprovarC2Component {
-  inputCode: string = ''; // Vinculado al input de HTML
-  message: string = ''; // Mensaje para mostrar al usuario
+  inputCode: string = '';
+  message: string = '';
 
   constructor(private socketService: SocketService) {}
 
@@ -24,13 +22,14 @@ export class ComprovarC2Component {
     }
 
     const isValid = await this.socketService.validateCode(this.inputCode);
+    this.message = isValid
+      ? '¡Código correcto! Redirigiendo al vídeo...'
+      : 'Código incorrecto. Inténtalo de nuevo.';
+
     if (isValid) {
-      this.message = '¡Código correcto! Redirigiendo al vídeo...';
       setTimeout(() => {
-        window.location.href = 'http://localhost:4200'; // Redirige al proyecto principal
+        window.location.href = 'http://localhost:4200';
       }, 2000);
-    } else {
-      this.message = 'Código incorrecto. Inténtalo de nuevo.';
     }
   }
 }
