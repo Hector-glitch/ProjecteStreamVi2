2c9419eab7128c3436db1308c6e4c3542358c5d3
Héctor <hector.vinas@institutvidreres.cat>
2024-12-05 18:06:32 +0100

    acabat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {ComprovarC2Component} from './comprovar-c2/comprovar-c2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ComprovarC2Component],
+  imports: [ComprovarC2Component],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
